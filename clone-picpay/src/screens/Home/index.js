da27bca55c8942c8e4b57d886ff69f00982a0dc3
Lucas Rezende <lucas.rezende.2000@gmail.com>
2020-05-02 23:47:09 -0300

    home page

--- clone-picpay/src/screens/Home/index.js
+++ clone-picpay/src/screens/Home/index.js
@@ -1,6 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons'
+
+import Sugestion from '../../components/Sugestions'
+import Activities from '../../components/Activities'
+import Tips from '../../components/Tips'
+import Banner from '../../components/Banner'
+
+
+import { Wrapper, Container, Header, BalanceContainer, BalanceTitle, Balance } from './style'
 
 export default function Home() {
-    return <Text>Home</Text>;
+    return (
+        <Wrapper>
+            <Container>
+                <Header>
+                    <MaterialCommunityIcons name="qrcode-scan" size={30} color="#18c85e" />
+
+                    <BalanceContainer>
+                        <BalanceTitle>Meu saldo:</BalanceTitle>
+                        <Balance>R$ 0,00</Balance>
+                    </BalanceContainer>
+
+                    <AntDesign name="gift" size={30} color="#10c86e" />
+                </Header>
+
+                <Sugestion />
+                <Activities />
+                <Tips />
+                <Banner />
+
+            </Container>
+        </Wrapper>
+    );
 }
